9dd2125b1591d2faf4f7c9448f2f4fc389774a22
Matt Popovich <popovich.matt@gmail.com>
2024-02-24 03:05:18 -0500

    Cleaned up script

--- compare_adobe_dng_converter_arguments.sh
+++ compare_adobe_dng_converter_arguments.sh
@@ -7,22 +7,19 @@
 # GOPR0925.GPR
 # compareDNGarguments.sh
 
-# WARNING: This script will move things around in that folder.
-#          Only use on in a folder that contains deleteable files
+# WARNING: This script will be creating (and destroying) a .DNG folder
 
 # Author: Matt Popovich (mattpopovich.com)
 
+# Stop running script if any command fails
+set -e
 
-# Move files into a GPR folder
-mkdir GPR
-mv *.GPR GPR
-
-# Create a DNG folder
-mkdir DNG
+# Create a (hidden) .DNG folder to store our converted files
+mkdir .DNG
 
 # Will pass the argument given to Adobe DNG Converter
 function test_flags(){
-    /Applications/Adobe\ DNG\ Converter.app/Contents/MacOS/Adobe\ DNG\ Converter $1 -d DNG GPR/*
+    /Applications/Adobe\ DNG\ Converter.app/Contents/MacOS/Adobe\ DNG\ Converter $1 -d .DNG *.GPR
 }
 
 flags_array=(
@@ -41,10 +38,10 @@ flags_array=(
 start_time=$(ruby -e 'puts Time.now.to_f')  # https://serverfault.com/a/423642/453183
 test_flags "${flags_array[0]}" > /dev/null 2>/dev/null
 end_time=$(ruby -e 'puts Time.now.to_f')
-default_run_time=$(echo "$end_time - $start_time" | bc)
-default_file_sizes=$(ls -l DNG | awk '{sum += $5} END {print sum}')
+default_run_time=$(echo "${end_time} - ${start_time}" | bc)
+default_file_sizes=$(ls -l .DNG | awk '{sum += $5} END {print sum}')
 echo "Default = ${default_file_sizes}B, ${default_run_time}s"
-rm DNG/*
+rm .DNG/*
 
 # Get statistics for flags
 for flags in "${flags_array[@]}"; do
@@ -52,26 +49,29 @@ for flags in "${flags_array[@]}"; do
     start_time=$(ruby -e 'puts Time.now.to_f')  # https://serverfault.com/a/423642/453183
     test_flags "$flags" > /dev/null 2>/dev/null # Suppress GPU Warnings
     end_time=$(ruby -e 'puts Time.now.to_f')
-    run_time=$(echo "$end_time - $start_time" | bc)
+    run_time=$(echo "${end_time} - ${start_time}" | bc)
 
     # Calculate time differences
-    time_difference=$(echo "$run_time - $default_run_time" | bc)
-    percent_time_difference=$(echo "100 * $time_difference / $default_run_time" | bc)
+    time_difference=$(echo "${run_time} - ${default_run_time}" | bc)
+    percent_time_difference=$(echo "100 * ${time_difference} / ${default_run_time}" | bc)
     if (( $(echo "$time_difference >= 0" | bc -l) )); then
-        time_difference="+$time_difference"
-        percent_time_difference="+$percent_time_difference"
+        time_difference="+${time_difference}"
+        percent_time_difference="+${percent_time_difference}"
     fi
 
     # Calculate file size differences
-    file_size=$(ls -l DNG | awk '{sum += $5} END {print sum}')
-    size_difference=$(echo "$file_size - $default_file_sizes" | bc)
-    percent_size_difference=$(echo "100 * $size_difference / $default_file_sizes" | bc)
+    file_size=$(ls -l .DNG | awk '{sum += $5} END {print sum}')
+    size_difference=$(echo "${file_size} - ${default_file_sizes}" | bc)
+    percent_size_difference=$(echo "100 * ${size_difference} / ${default_file_sizes}" | bc)
     if (( $(echo "$size_difference >= 0" | bc -l) )); then
-        size_difference="+$size_difference"
-        percent_size_difference="+$percent_size_difference"
+        size_difference="+${size_difference}"
+        percent_size_difference="+${percent_size_difference}"
     fi
 
     # Output statistics
-    echo "Using flags" $flags "= ${time_difference}s (${percent_time_difference}%), ${size_difference}B (${percent_size_difference}%) vs default"
-    rm DNG/*
+    echo "Using flags ${flags} = ${time_difference}s (${percent_time_difference}%), ${size_difference}B (${percent_size_difference}%) vs default"
+    rm .DNG/*
 done
+
+# Remove temporary folder
+rm -r .DNG
--- reset.sh
+++ reset.sh
@@ -4,5 +4,6 @@ rm -f *.JPG
 rm -rf GPR
 rm -rf JPG
 rm -rf DNG
+rm -rf .DNG
 
 cp original/* .
