27771511edd9e0e1f3f298b9e3e4aef5403c7e70
Matt Popovich <popovich.matt@gmail.com>
2024-02-24 01:04:51 -0500

    Made this script into a function

--- compareDNGsettings.sh
+++ compareDNGsettings.sh
@@ -13,59 +13,29 @@ mv *.GPR GPR
 # Create a DNG folder
 mkdir DNG
 
-# TODO: make this into a function
+# Will pass the argument given to Adobe DNG Converter
+function test_flags(){
+    /Applications/Adobe\ DNG\ Converter.app/Contents/MacOS/Adobe\ DNG\ Converter $1 -d DNG GPR/*
+}
+
+flags_array=(
+    "-c -p1 -cr7.1 -dng1.4"
+    "-u -p1 -cr7.1 -dng1.4"
+    "-l -p1 -cr7.1 -dng1.4"
+    "-c -e -p1 -cr7.1 -dng1.4"
+    "-c -p0 -cr7.1 -dng1.4"
+    "-c -p2 -cr7.1 -dng1.4"
+    "-c -p1 -fl -cr7.1 -dng1.4"
+    "-c -p1 -lossy -cr7.1 -dng1.4"
+    "-c -mp -p1 -cr7.1 -dng1.4"
+)
+
 # TODO: Remove the adobe dng output
 # TODO: Add file size difference (percentage) compared to default arguments
-flags="-c -p1 -cr7.1 -dng1.4"
-/Applications/Adobe\ DNG\ Converter.app/Contents/MacOS/Adobe\ DNG\ Converter $flags -d DNG GPR/* > /dev/null
-echo "Using flags" $flags
-ls -l DNG
-rm DNG/*
 
-flags="-u -p1 -cr7.1 -dng1.4"
-/Applications/Adobe\ DNG\ Converter.app/Contents/MacOS/Adobe\ DNG\ Converter $flags -d DNG GPR/* > /dev/null
-echo "Using flags" $flags
-ls -l DNG
-rm DNG/*
-
-flags="-l -p1 -cr7.1 -dng1.4"
-/Applications/Adobe\ DNG\ Converter.app/Contents/MacOS/Adobe\ DNG\ Converter $flags -d DNG GPR/* > /dev/null
-echo "Using flags" $flags
-ls -l DNG
-rm DNG/*
-
-flags="-c -e -p1 -cr7.1 -dng1.4"
-/Applications/Adobe\ DNG\ Converter.app/Contents/MacOS/Adobe\ DNG\ Converter $flags -d DNG GPR/* > /dev/null
-echo "Using flags" $flags
-ls -l DNG
-rm DNG/*
-
-flags="-c -p0 -cr7.1 -dng1.4"
-/Applications/Adobe\ DNG\ Converter.app/Contents/MacOS/Adobe\ DNG\ Converter $flags -d DNG GPR/* > /dev/null
-echo "Using flags" $flags
-ls -l DNG
-rm DNG/*
-
-flags="-c -p2 -cr7.1 -dng1.4"
-/Applications/Adobe\ DNG\ Converter.app/Contents/MacOS/Adobe\ DNG\ Converter $flags -d DNG GPR/* > /dev/null
-echo "Using flags" $flags
-ls -l DNG
-rm DNG/*
-
-flags="-c -p1 -fl -cr7.1 -dng1.4"
-/Applications/Adobe\ DNG\ Converter.app/Contents/MacOS/Adobe\ DNG\ Converter $flags -d DNG GPR/* > /dev/null
-echo "Using flags" $flags
-ls -l DNG
-rm DNG/*
-
-flags="-c -p1 -lossy -cr7.1 -dng1.4"
-/Applications/Adobe\ DNG\ Converter.app/Contents/MacOS/Adobe\ DNG\ Converter $flags -d DNG GPR/* > /dev/null
-echo "Using flags" $flags
-ls -l DNG
-rm DNG/*
-
-flags="-c -mp -p1 -cr7.1 -dng1.4"
-/Applications/Adobe\ DNG\ Converter.app/Contents/MacOS/Adobe\ DNG\ Converter $flags -d DNG GPR/* > /dev/null
-echo "Using flags" $flags
-ls -l DNG
-rm DNG/*
+for flag in "${flags_array[@]}"; do
+    test_flags "$flags"
+    echo "Using flags" $flags
+    ls -l DNG
+    rm DNG/*
+done
